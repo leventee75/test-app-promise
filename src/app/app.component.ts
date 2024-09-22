a09682cd0a5284c115b930d231799452a830d01b
Levi <leventee75@gmail.com>
2024-09-22 11:01:43 +0200

    implements test functions

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -8,6 +8,38 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'test-app-promise';
+
+  ngOnInit() {
+    const promise = new Promise((resolve, reject) => {
+      this.title = 'test-app-promise_v2';
+      console.log('v2');
+      resolve('all is ok');
+      //reject('failed');
+    })
+    promise
+    .then((result) => { 
+      this.title = 'test-app-promise_v30'; 
+      console.log(result);
+      return result + ' then1';
+    })
+    .then((result) => { 
+      this.title = 'test-app-promise_v31'; 
+      console.log(result);
+      return result + ' then2';
+    })
+    .then((result) => { 
+      this.title = 'test-app-promise_v32'; 
+      console.log(result);
+      return result + ' then3';
+    })
+    .catch(() => { 
+      this.title = 'test-app-promise_v4';
+      console.log('catch');
+    })
+    .finally(() => {
+      console.log('promise is ready');
+    })
+  }
 }
